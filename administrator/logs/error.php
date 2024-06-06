7a7a7ad41eea719953999e19522643a833c7c4da
Felix Cordero Jr. <corderofelix101@gmail.com>
2024-06-07 03:40:09 +0800

    test run

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-06-06 17:09:05 UTC
+#Software: Joomla! 5.1.1 Stable [ Kudumisha ] 28-May-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-06-06T17:09:05+00:00	INFO 112.205.144.62	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-06-06T17:09:59+00:00	INFO 112.205.144.62	joomlafailure	Username and password do not match or you do not have an account yet.
